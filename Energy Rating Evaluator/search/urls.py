844f67a101cadcad40c3286541221ba86e832dc6
Saurabh Atreya <112821423+atrey-a@users.noreply.github.com>
2024-08-23 00:17:50 +0530

    feat: start on invoice upload

--- Energy Rating Evaluator/search/urls.py
+++ Energy Rating Evaluator/search/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.search_products, name='search'),
+    path('/upload', views.upload_invoice, name='upload')
 ]
--- Energy Rating Evaluator/search/views.py
+++ Energy Rating Evaluator/search/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from utils.search_db import find_closest_match
 from utils.amazon import get_first_google_result,scrape_amazon_product
 from . import client
+from utils.reading_invoice import process_invoice
 
 def search_products(request):
     if request.method == 'POST':
@@ -10,5 +11,25 @@ def search_products(request):
         features = scrape_amazon_product(first_url)
         rating = client.extract_rating(features)
         C = "Yes" if int(rating)>=3 else "No"
+        return render(request, 'search/results.html', {'products': [[query,rating,C]], 'query': query})
+    return render(request, 'search/search.html')
+
+def upload_invoice(request):
+    if request.method == 'POST':
+        if 'image' not in request.FILES:
+            return JsonResponse({'error': 'No image uploaded'}, status=400)
+        image_file = request.FILES['image']
+        image_bytes = image_file.read()
+        np_arr = np.frombuffer(image_bytes, np.uint8)
+        img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        names = process_invoice(img)
+        names = [f"{k} {v}" for k,v in names.items()]
+        results = []
+        for name in names:
+            first_url = get_first_google_result(name)
+            features = scrape_amazon_product(first_url)
+            rating = client.extract_rating(features)
+            C = "Yes" if int(rating)>=3 else "No"
+            results.append([name, rating, C])
         return render(request, 'search/results.html', {'products': [[query,rating,C]], 'query': query})
     return render(request, 'search/search.html')
